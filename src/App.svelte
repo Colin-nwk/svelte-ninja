0dfbaf0a0c607a5954676e6fee6f05e93e3d1c33
Collins Nwoko <collinsnwoko05@gmail.com>
2021-08-13 23:13:03 +0100

    css and conditional styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Modal from "./Modal.svelte";
   let people = [
     { name: "John", beltcolor: "black", age: 30, id: 1 },
     { name: "Mario", beltcolor: "red", age: 20, id: 2 },
@@ -11,21 +12,23 @@
   let num = 5;
 </script>
 
+<!-- 
 {#if num > 20}
   <p>Greater than 20 people</p>
 {:else if num > 5}
   <p>Greater than 5 people</p>
 {:else}
   <p>Not greater than 5 people</p>
-{/if}
+{/if} -->
+<Modal />
 <main>
   {#each people as person (person.id)}
     <div>
-      <h2>{person.name}</h2>
+      <h4>{person.name}</h4>
       {#if person.beltcolor === "black"}
         <p><strong>Master Ninja</strong></p>
       {/if}
-      <h4>{person.beltcolor} belt</h4>
+      <h5>{person.beltcolor} belt</h5>
       <p>{person.age} years old</p>
       <button on:click={() => handleClick(person.id)}>delete</button>
     </div>
@@ -42,12 +45,12 @@
     margin: 0 auto;
   }
 
-  h2 {
+  /* h2 {
     color: #ff3e00;
     text-transform: uppercase;
     font-size: 1em;
     font-weight: 100;
-  }
+  } */
 
   @media (min-width: 640px) {
     main {
---
+++ src/Modal.svelte
@@ -0,0 +1,36 @@
+<script>
+  let showModal = true;
+  let isPromo = false;
+</script>
+
+{#if showModal}
+  <div class="backdrop" class:promo={isPromo}>
+    <div class="modal">
+      <p>Sign up for offers</p>
+    </div>
+  </div>
+{/if}
+
+<main />
+
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.8);
+  }
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background-color: white;
+  }
+
+  .promo .modal {
+    background-color: red;
+    color: white;
+  }
+</style>
